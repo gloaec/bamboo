39b8ea7209e8b54694baac9c7f7bf067a6818460
Ghislain Loaec <gloaec@cadoles.com>
2014-02-21 15:31:47 +0100

    Error traceback

--- bamboo/__init__.py
+++ bamboo/__init__.py
@@ -3,12 +3,15 @@ from __future__ import absolute_import
 
 import os
 import sys
+import logging
 
 from flask import Flask
 
 from .managers.manager import Manager
 from .utils import basedir, appdir, find_subclasses
 # --------------------
+
+logging.basicConfig(level=logging.DEBUG)
 
 def main(argv=None, prog=None, **kwargs):
     _basedir = basedir()
@@ -22,7 +25,6 @@ def main(argv=None, prog=None, **kwargs):
         manager.add_command("new", NewApplication)
     else:
         # Load Application context 
-
         if _basedir != os.getcwd():
             print "(in %s)" % _basedir
 
@@ -30,11 +32,9 @@ def main(argv=None, prog=None, **kwargs):
 	_appname = os.path.dirname(_appdir)
     
         sys.path.append(_basedir)
-	try:
-            bb_app_manager = __import__('manage')
-	    manager = bb_app_manager.manager
-	except Exception, e:
-	    print e
+        bb_app_manager = __import__('manage')
+	manager = bb_app_manager.manager
+
         #execfile(os.path.abspath(os.path.join(_basedir, 'manage.py')), globals())
     manager.run()
 
